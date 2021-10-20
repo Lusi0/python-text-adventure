c2fbc1bbcda9b2613fc4f6bf4be7dd689db2e07d
Lusi0 <ItsLusio@gmail.com>
2021-10-20 11:01:14 -0700

    Win contition added for protoype testing
    continual testing will be conducted

--- project/main.py
+++ project/main.py
@@ -2,5 +2,9 @@ from game_object import *
 from text_parser import textparser
 
 #gameLoop
+print("Your goal is to drop a key in the hallway!")
 while True:
     textparser(gameobjects['john'])
+    if "key" in gameobjects["hallway"].interactables:
+        print("You Win!")
+        break
